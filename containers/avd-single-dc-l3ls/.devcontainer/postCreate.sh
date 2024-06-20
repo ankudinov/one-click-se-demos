7431175bd6b865dc536aba30fb52255abda82185
pa <pa@arista.com>
2024-06-20 00:14:43 +0000

    change platform after copying inventory

--- containers/avd-single-dc-l3ls/.devcontainer/postCreate.sh
+++ containers/avd-single-dc-l3ls/.devcontainer/postCreate.sh
@@ -7,14 +7,13 @@ grep -rl '{{gh.repo_name}}' . --exclude-dir .git | xargs sed -i 's/'{{gh.repo_na
 grep -rl '{{gh.org_name}}' . --exclude-dir .git | xargs sed -i 's/'{{gh.org_name}}'/'"${GITHUB_REPOSITORY%%/*}"'/g'
 grep -rl '{{gh.repository}}' . --exclude-dir .git | xargs sed -i 's@'{{gh.repository}}'@'"${GITHUB_REPOSITORY}"'@g'
 
-# change platform to cEOS
-grep -rl 'platform: vEOS-lab' . --exclude-dir .git | xargs sed -i 's@'platform: vEOS-lab'@'"platform: cEOS"'@g'
-
 ardl get eos --image-type cEOS --version ${CEOS_LAB_VERSION}  --import-docker
 
 # copy AVD inventory
 mkdir ${CONTAINERWSF}/avd_inventory/
 cp -r /home/avd/.ansible/collections/ansible_collections/arista/avd/examples/single-dc-l3ls/* ${CONTAINERWSF}/avd_inventory/
+# change platform to cEOS
+grep -rl 'platform: vEOS-lab' . --exclude-dir .git | xargs sed -i 's@'platform: vEOS-lab'@'"platform: cEOS"'@g'
 
 # init demo dir as Git repo if requested for this demo env
 if ${GIT_INIT}; then
